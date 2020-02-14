f0dde20a7bf1c9a76b951f0577e1e00ddf9fb4bf
Kristopher Stewart <stewart.kristopher.d@gmail.com>
2020-02-14 11:05:55 -0800

    experimented with database

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button, TextInput, FlatList} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 const Realm = require('realm');
 
@@ -18,7 +18,7 @@ const Realm = require('realm');
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {realm: null};
+    this.state = {realm: null, count: 0};
   }
 
   componentDidMount() {
@@ -44,15 +44,131 @@ export default class App extends Component {
   }
 
   render() {
-    const info = this.state.realm
-      ? 'Number of dogs in this Realm that are young adults: ' +
-        this.state.realm.objects('Dog').filtered('age > 2').length
+    let info = this.state.realm
+      ? 'Number of dogs in this Realm: ' +
+        this.state.realm.objects('Dog').length
       : 'Loading...';
 
     return (
       <View>
         <Text>{info}</Text>
+        <DatabaseInput
+          realm={this.state.realm}
+          onSubmit={() => {
+            this.setState({count: this.state.count + 1});
+            console.log('submitted record');
+          }}
+          onDelete={() => {
+            this.setState({count: 0});
+            console.log('deleted records');
+          }}
+        />
+        <List realm={this.state.realm} />
       </View>
     );
+  }
+}
+
+function Item({name, age}) {
+  console.log(`${name}, ${age}`);
+  return (
+    <View>
+      <Text>
+        Name: {name} Age: {age}
+      </Text>
+    </View>
+  );
+}
+class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }
+
+  render() {
+    let realm = this.props.realm;
+    if (realm) {
+      const dogs = realm.objects('Dog');
+      if (dogs) {
+        return (
+          <View>
+            <FlatList
+              data={dogs}
+              renderItem={({item}) => <Item name={item.name} age={item.age} />}
+              keyExtractor={d => d.name}
+            />
+          </View>
+        );
+      }
+    }
+    return null;
+  }
+}
+
+class DatabaseInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '', age: ''};
+  }
+  render() {
+    return (
+      <View>
+        <Text>Enter input to add to the database:</Text>
+        <TextInput
+          placeholder="Enter a dog name"
+          onChangeText={name => this.setState({name})}
+          value={this.state.name}
+        />
+        <TextInput
+          placeholder="Enter a dog age"
+          onChangeText={age => this.setState({age})}
+          value={this.state.age}
+        />
+        <Button
+          onPress={() => {
+            this.submit();
+            this.setState({name: ''});
+            this.setState({age: ''});
+          }}
+          title="Submit Record"
+        />
+        <Button
+          onPress={() => {
+            this.delete();
+          }}
+          title="Delete all"
+        />
+      </View>
+    );
+  }
+
+  submit() {
+    let realm = this.props.realm;
+    if (
+      realm &&
+      !realm.isClosed &&
+      this.state.name !== '' &&
+      this.state.age !== ''
+    ) {
+      realm.write(() => {
+        realm.create('Dog', {
+          name: this.state.name,
+          age: parseInt(this.state.age, 10),
+        });
+        this.props.onSubmit();
+      });
+    } else {
+      console.log('db closed!!!!!!!!!!!!!!!');
+    }
+  }
+
+  delete() {
+    const realm = this.props.realm;
+    if (realm && !realm.isClosed) {
+      realm.write(() => {
+        realm.deleteAll();
+        this.props.onDelete();
+      });
+    }
   }
 }
