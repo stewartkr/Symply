ea915bcbcba429b121c739810ead9236d944bc78
Kristopher Stewart <stewart.kristopher.d@gmail.com>
2020-02-14 11:13:16 -0800

    updated schema format

--- Schemas.js
+++ Schemas.js
@@ -10,133 +10,146 @@ const dataEnum = {
   },
 };
 
-const dataSchemas = {
-  Treatment: {
-    name: 'Treatment',
-    properties: {
-      name: 'string',
-      medication: 'bool',
-      dose: 'int?',
-      doseUnit: 'string?',
-      tags: 'string?[]',
-      start: 'date?',
-      end: 'date?',
-      status: {type: 'int', default: 0},
-    },
-  },
-
-  Symptom: {
-    name: 'Symptom',
-    properties: {
-      name: 'string',
-      tags: 'string?[]',
-      status: {type: 'int', default: 0},
-    },
-  },
-
-  Incident: {
-    name: 'Incident',
-    properties: {
-      symptoms: 'Symptom?[]',
-      providers: 'Provider?[]',
-      treatments: 'Treatment?[]',
-      notes: 'Note?[]',
-      severity: 'int',
-      tags: 'string?[]',
-      logDate: 'date',
-    },
-  },
-
-  Reflection: {
-    name: 'Reflection',
-    properties: {
-      sleepDuration: 'float',
-      sleepQuality: 'int',
-      diet: 'int',
-      activities: 'Activity[]',
-      notes: 'Note?[]',
-      logDate: 'date',
-    },
-  },
-
-  Activity: {
-    name: 'Activity',
-    properties: {
-      name: 'string',
-      status: {type: 'int', default: 0},
-    },
-  },
-
-  Reminder: {
-    name: 'Reminder',
-    properties: {
-      tasks: 'Task?[]',
-      text: {type: 'string', default: ''},
-      schedule: 'string',
-    },
-  },
-
-  ReminderOccurrence: {
-    name: 'ReminderOccurrence',
-    properties: {
-      status: {type: 'int', default: 0},
-      parent: 'Reminder',
-    },
-  },
-
-  Task: {
-    name: 'Task',
-    properties: {
-      todo: 'string',
-      treatment: 'Treatment?',
-    },
-  },
-
-  Tag: {
-    name: 'Tag',
-    properties: {
-      name: 'string',
-      associates: 'string[]',
-      status: {type: 'int', default: 0},
-    },
-  },
-
-  Provider: {
-    name: 'Provider',
-    properties: {
-      firstName: 'string',
-      lastName: 'string',
-      address: 'string?',
-      contacts: 'Contact?[]',
-      occupation: 'string?',
-      photo: 'string?',
-    },
-  },
-
-  Contact: {
-    name: 'Contact',
-    properties: {
-      type: 'string',
-      content: 'string',
-    },
-  },
-
-  Appointment: {
-    name: 'Appointment',
-    properties: {
-      provider: 'Provider',
-      time: 'date',
-      notes: 'Note?[]',
-    },
-  },
-
-  Note: {
-    name: 'Note',
-    properties: {
-      text: 'string',
-      logDate: 'date',
-    },
+const Treatment = {
+  name: 'Treatment',
+  properties: {
+    name: 'string',
+    medication: 'bool',
+    dose: 'int?',
+    doseUnit: 'string?',
+    tags: 'string?[]',
+    start: 'date?',
+    end: 'date?',
+    status: {type: 'int', default: 0},
   },
 };
 
-export {dataEnum, dataSchemas};
+const Symptom = {
+  name: 'Symptom',
+  properties: {
+    name: 'string',
+    tags: 'string?[]',
+    status: {type: 'int', default: 0},
+  },
+};
+
+const Incident = {
+  name: 'Incident',
+  properties: {
+    symptoms: 'Symptom?[]',
+    providers: 'Provider?[]',
+    treatments: 'Treatment?[]',
+    notes: 'Note?[]',
+    severity: 'int',
+    tags: 'string?[]',
+    logDate: 'date',
+  },
+};
+
+const Reflection = {
+  name: 'Reflection',
+  properties: {
+    sleepDuration: 'float',
+    sleepQuality: 'int',
+    diet: 'int',
+    activities: 'Activity[]',
+    notes: 'Note?[]',
+    logDate: 'date',
+  },
+};
+
+const Activity = {
+  name: 'Activity',
+  properties: {
+    name: 'string',
+    status: {type: 'int', default: 0},
+  },
+};
+
+const Reminder = {
+  name: 'Reminder',
+  properties: {
+    tasks: 'Task?[]',
+    text: {type: 'string', default: ''},
+    schedule: 'string',
+  },
+};
+
+const ReminderOccurrence = {
+  name: 'ReminderOccurrence',
+  properties: {
+    status: {type: 'int', default: 0},
+    parent: 'Reminder',
+  },
+};
+
+const Task = {
+  name: 'Task',
+  properties: {
+    todo: 'string',
+    treatment: 'Treatment?',
+  },
+};
+
+const Tag = {
+  name: 'Tag',
+  properties: {
+    name: 'string',
+    associates: 'string[]',
+    status: {type: 'int', default: 0},
+  },
+};
+
+const Provider = {
+  name: 'Provider',
+  properties: {
+    firstName: 'string',
+    lastName: 'string',
+    address: 'string?',
+    contacts: 'Contact?[]',
+    occupation: 'string?',
+    photo: 'string?',
+  },
+};
+
+const Contact = {
+  name: 'Contact',
+  properties: {
+    type: 'string',
+    content: 'string',
+  },
+};
+
+const Appointment = {
+  name: 'Appointment',
+  properties: {
+    provider: 'Provider',
+    time: 'date',
+    notes: 'Note?[]',
+  },
+};
+
+const Note = {
+  name: 'Note',
+  properties: {
+    text: 'string',
+    logDate: 'date',
+  },
+};
+
+export {
+  dataEnum,
+  Treatment,
+  Symptom,
+  Incident,
+  Reflection,
+  Activity,
+  Reminder,
+  ReminderOccurrence,
+  Tag,
+  Appointment,
+  Contact,
+  Note,
+  Provider,
+  Task,
+};
